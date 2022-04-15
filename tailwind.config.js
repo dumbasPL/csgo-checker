9e4fd3f5d28336fd1f3dc75310cf9a3849f5184b
nezu <29180158+dumbasPL@users.noreply.github.com>
2022-04-15 23:03:11 +0200

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.red
+      },
+    },
   },
   plugins: [],
 }
